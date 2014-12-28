81d45680611cd4af012aa602692089232dc792b6
MattSingman <math2@live.com>
2014-12-28 15:54:27 -0500

    Works slightly better with other numbers, not perfect

--- FBLA2014/MenuItem.cpp
+++ FBLA2014/MenuItem.cpp
@@ -15,8 +15,8 @@ SDL_Surface* MenuItem::placeOnScreen(int x, int y, int numPrev, int distanceAllo
 	dstrect.x = x - (WIDTH/2); //compensates for SDL_Rect taking values at left corner, so the value passed in is the center of the item
 	//TODO dstrect.y
 	dstrect.y = y +
-		(numPrev*distanceAllotted);// + //Base distance + distanceallotted for each other TODO LOOK AT THIS LATER
-		//(distanceAllotted - getItemHeight()) / 2;
+		(numPrev*distanceAllotted) + //Base distance + distanceallotted for each other TODO LOOK AT THIS LATER
+		(distanceAllotted - getItemHeight()) / 2;
 	SDL_Surface* surface = IMG_Load(path); //Load menu background
 	surface = SDL_ConvertSurface(surface, getContainerWindow() -> getSurface() -> format, NULL); //optimize image 
 	SDL_BlitSurface(surface, NULL, getContainerWindow()->getSurface(), &dstrect); //Put menu background on screen. Change dstrect to a pointer TODO: Make it a pointer all the way through?
