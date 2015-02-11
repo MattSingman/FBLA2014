4ffe465fc8afb1f1d9d8b354401d694aeb5b9618
MattSingman <math2@live.com>
2015-02-10 20:06:42 -0500

    More art more waves

--- FBLA2014/game.cpp
+++ FBLA2014/game.cpp
@@ -28,7 +28,8 @@ void game::showTutorial() {
 		"turret to deal damage to an enemy. If the enemy makes it past your defenses, your computer takes damage. Carefully watch your credits!";
 	messageBoxes.push_back(MessageBox(secondTextString.c_str(), renderer, font));
 
-
+	std::string thirdTextString = std::string("When a file is under the gaze of the scanner, if the file is malicious, you'll see the file turn red, and turrets can and will shoot it.");
+	messageBoxes.push_back(MessageBox(thirdTextString.c_str(), renderer, font));
 
 }
 
@@ -63,22 +64,47 @@ void game::waveComplete() {
 	}
 	else if (wave == 2) {
 		enemiesLeft = 10;
+		std::string textString = std::string("You downloaded some Word documents! Be careful, Word documents can include macros, which can, if malicious, replace benign macros on your computer and execute unwanted actions");
+		messageBoxes.push_back(MessageBox(textString.c_str(), renderer, font));
+
+		File newEnemy1 = File("../art/wordDocumentEnemy.bmp", "../art/wordDocumentFriend.bmp", true,
+			1, 5, 5, renderer);
+		File newEnemy2 = File("../art/wordDocumentEnemy.bmp", "../art/wordDocumentFriend.bmp", false,
+			1, 5, 5, renderer);
+		for (int i = 0; i < wave; ++i) {
+			fileChoices.push_back(newEnemy1);
+			fileChoices.push_back(newEnemy2);
+		}
+		
+		
+	}
+	else if (wave == 3) {
+		enemiesLeft = 10;
 		std::string textString = std::string("The next enemy attacking your computer is a browser hijacker, which is attempting to take control of your browser and redirect your web queries!");
 		messageBoxes.push_back(MessageBox(textString.c_str(), renderer, font));
 
 		File newEnemy1 = File("../art/browserEnemy.bmp", "../art/browserFriend.bmp", true,
-			1, 5, 5, renderer);
-		File newEnemy2 = File("../art/browserEnemy.bmp", "../art/browserEnemy.bmp", false,
-			1, 5, 5, renderer);
-		fileChoices.push_back(newEnemy1);
-		fileChoices.push_back(newEnemy2);
-		fileChoices.push_back(newEnemy1);
-		fileChoices.push_back(newEnemy2);
-	}
-	else if (wave == 3) {
+			1.5, 7, 7, renderer);
+		File newEnemy2 = File("../art/browserEnemy.bmp", "../art/browserFriend.bmp", false,
+			1.5, 7, 7, renderer);
+		for (int i = 0; i < wave; ++i) {
+			fileChoices.push_back(newEnemy1);
+			fileChoices.push_back(newEnemy2);
+		}
 
 	}
 	else if (wave == 4) {
+		enemiesLeft = 10;
+		std::string textString = std::string("It looks like you downloaded an executable file. Be wary, some executables may be direct action viruses, or malware that executes upon execution.");
+		
+		File newEnemy1 = File("../art/directActionEnemy.bmp", "../art/directActionFriend.bmp", true,
+			1, 5, 5, renderer);
+		File newEnemy2 = File("../art/directActionEnemy.bmp", "../art/directActionFriend.bmp", false,
+			1, 5, 5, renderer);
+		for (int i = 0; i < wave; ++i) {
+			fileChoices.push_back(newEnemy1);
+			fileChoices.push_back(newEnemy2);
+		}
 
 	}
 	else if (wave == 5) {
